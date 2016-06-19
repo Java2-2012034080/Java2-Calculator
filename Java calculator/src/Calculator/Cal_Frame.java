9d70b9d2e4988fe012159a9eb2d527b5376df2d2
youngmin <ghjk9306@naver.com>
2016-06-19 22:14:33 +0900

    최종본

--- Java calculator/src/Calculator/Cal_Frame.java
+++ Java calculator/src/Calculator/Cal_Frame.java
@@ -10,7 +10,7 @@ import java.awt.GridLayout;
  import javax.swing.JFrame;
  import javax.swing.JPanel;
  import javax.swing.JScrollPane;
- import javax.swing.JTextArea;
+ import javax.swing.JTextArea;//
  
  public class Cal_Frame extends JFrame implements ActionListener {
  
